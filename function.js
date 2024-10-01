ceff54d9c9348ec04b5515dd340885978b87e4a8
priyanka571 <priyankapandit0012@gmail.com>
2024-10-01 20:33:11 +0530

    feat: functions

---
+++ function.js
@@ -0,0 +1,39 @@
+// jsUser ko as a object define karna padega tabhi function run hoga
+// const jsUser={
+//     name:"priya"
+// }
+// jsUser.greeting=function(){
+//     console.log("hello Js User");
+    
+// }
+// console.log(jsUser.greeting())
+// const name="Priya"
+// const jsUser="hello"
+// jsUser.greet=function(){
+//     console.log(`hello Js User,${name}`);
+// }
+
+// console.log(jsUser.greet())
+
+// ++++++++++function without object++++++
+// function hi(){
+//     console.log("Say hii");
+// }
+// hi();
+
+// +++++++++Singleton object++++++
+// Key Characteristics of a Singleton Object:
+// Single instance: Only one instance of the object exists.
+// Global access: The instance can be accessed from anywhere in the program.
+// const tinderUser={}
+// tinderUser.id="123abc"
+// tinderUser.name="sammy"
+// tinderUser.isLoggedIn=false
+// console.log(tinderUser)
+
+
+// ++++++++non singleton object+++
+// Key Characteristics of Non-Singleton Objects:
+// Multiple instances: You can create as many instances as needed.
+// Independent state: Each instance has its own separate state and properties.
+// No global access: Unlike singletons, non-singleton objects do not enforce a global instance and are typically instantiated when needed.
--- practise_basics.js
+++ practise_basics.js
@@ -63,8 +63,8 @@
 // // console.log(myCreatedDate.getTime());
 // console.log(Math.floor(Date.now()/1000))
 
-// let newDate = new Date()
+ let newDate = new Date()
 //  console.log(newDate.getDay())
-// newDate.toLocaleString('default',{
-//     weekday:"Long"
-// })
+console.log(newDate.toLocaleString('default',{
+    weekday:"long"
+}))
